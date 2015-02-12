ba0ec4bd8040becf5c197bb472a9d180918d0ef9
juanvallejo <juan.vallejo.12@cnu.edu>
2015-02-12 14:24:56 -0500

    update readme, add database support

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 The OpenShift `nodejs` cartridge documentation can be found at:
 
 http://openshift.github.io/documentation/oo_cartridge_guide.html#nodejs
+
+###Endpoints
+
+All requests originate from the following URL
+
+```
+http://lendme-dmnhackathon.rhcloud.com/
+```
+
+The server handles data based on the endpoint you request. To put or pull data from the database, use the format below:
+
+```
+/action/<login|post|put>
+```
+
+Requests to any endpoints using the `/put` format must be done with a `POST` method. Think of them as actions that require data to be "pushed" to the server / database in order to obtain a result
+
+- `login` takes two parameters
+    - `username` String containing a user identifier
+    - `password` String containing a user password
+
+- `put` takes two parameters
+    - `table` database table where row will 
+
+Requests to any endpoints using the `/get` must be done with a `GET` method. Think of them as actions that simply request specific data. Formats for this action are shown below:
+
+- `<table_name>` String containing the name of the table where the item resides
+- `<identifier_type>` How you plan on retrieving the data, by id, username, etc...
+- `<identifier>` String containing the identifier of the item you wish to retrieve from the table (user_id, user_username)
--- server.js
+++ server.js
@@ -23,6 +23,13 @@ var SERVER_HEAD_ERROR       = 500;
 var SERVER_RES_OK           = '200. Server status: OK';
 var SERVER_RES_NOTFOUND     = '404. The file you are looking for could not be found.';
 var SERVER_RES_ERROR        = '500. An invalid request was sent to the server.';
+var SERVER_RES_POST_ERROR   = '500. Invalid method used. Action expects a POST request.';
+
+var MYSQL_HOST              = process.env.OPENSHIFT_MYSQL_DB_HOST || 'localhost';
+var MYSQL_PORT              = process.env.OPENSHIFT_MYSQL_DB_PORT || '3306';
+var MYSQL_USERNAME          = 'admin8AnEDyb';
+var MYSQL_PASSWORD          = 'Me4VU3l42uZS';
+var MYSQL_DATABASE          = 'lendme';
 
 // import node.js packages
 
@@ -30,6 +37,7 @@ var fs              = require('fs');
 var http            = require('http');
 var https           = require('https');
 var url             = require('url');
+var mysql           = require('mysql');
 
 // begin application declarations
 
@@ -56,8 +64,172 @@ var dictionaryOfMimeTypes = {
 var dictionaryOfRoutes  = {
 
     '/'                             : 'index.html',
-    '/post'                         : handleRequestAsPostData
+    '/post'                         : handleRequestAsPostData,
+    '/action/login'                 : handleRequestAsActionLogin
 
+};
+
+/**
+ * define mysql connection object. Handle all connections to the database
+**/
+var database = {
+
+    // define mysql object properties
+    connection          :   null,               // holds the connection object to the mysql server or null if not connected
+    hasData             :   false,              // flag indicating whether mysql database table contains any data
+    isBusy              :   false,              // flag indicating whether a mysql query is currently ongoing
+    isConnected         :   false,              // flag indicating whether a connection to mysql server has been established
+
+    /**
+     * creates and establishes a connection to
+     * the mysql server
+     *
+     * @param host      = {String} specifying mysql server address
+     * @param user      = {String} specifying database account username
+     * @param password  = {String} specifying database account password
+     * @param database  = {String} specifying the name of database to connect to
+    **/
+    connect : function(host, user, password, database) {
+        // check to see if previous connection exists, or @params for new connection are passed
+        if(!database.isConnected || (host && user && password)) {
+            // create connection blueprint
+            database.connection = mysql.createConnection({
+
+                host:           host || MYSQL_HOST,
+                user:           user || MYSQL_USERNAME,
+                pass:       password || MYSQL_PASSWORD,
+                database:   database || MYSQL_DATABASE
+
+            });
+
+            // create connection to server
+            database.connection.connect(function(error) {
+
+                // check to see if connection was successful
+                if(error) {
+                    return console.log('Error establishing a connection to the mysql server -> ' + error);;
+                }
+
+                console.log('<MySQL> successfully connected to mysql server');
+            });
+
+            // tell connection flag that connection was successful
+            database.isConnected = true;
+
+            // if new connection @params are given, or there is no previous connection,
+            // create one and return it
+            return database.connection;
+
+        } else {
+            // return existing connection to the database
+            return database.connection;
+        }
+    },
+
+    /**
+     * deletes entries from table where whereLogic applies
+     *
+     * @param mysqlTableName    = {Object}      entry object from local 'database' object
+     * @param whereLogic        = {String}      containing equality to use to target the selection of a specific row
+     * @param callback          = {Function}    to call after operation has completed successfully
+     *
+     * for data protection, if @param whereLogic is 'null', nothing is deleted / returned
+    **/
+    deleteFrom : function(mysqlTableName, whereLogic, callback) {
+        if(whereLogic) {
+            // perform query only if whereLogic has been passed
+            mysql.connect()
+                .query('DELETE FROM ' + mysqlTableName + ' WHERE ' + (whereLogic || '1 = 1'), callback);
+        } else {
+            // fail and exit function with error
+            callback.call(this, 'ERR: (mysqldatabasedeletionerror): no \'WHERE\' condition applies for selected logic.');
+        }
+    },
+
+    /**
+     * safely closes the mysql connection
+    **/
+    end : function() {
+        if(mysql.isConnected) {
+            // reset our flag to indicate no connection exists
+            mysql.isConnected = false;
+
+            // send close packet to server
+            mysql.connection.end();
+        }
+    },
+
+    /**
+     * inserts new entry to mysql database
+     *
+     * @param mysqlTableName    = {Object}      entry object from local 'database' object
+     * @param databaseColumns   = {Array}       containing names of mysql table columns to insert values into
+     * @param valuesToAdd       = {Array}       containing entry values to add
+     * @param callback          = {Function}    to call after operation has completed successfully
+    **/
+    insertInto : function(mysqlTableName, databaseColumns, valuesToAdd, callback) {
+        // our values to add have to be in quotes. Add them to each value on the list
+        valuesToAdd.forEach(function(value, index) {
+            valuesToAdd[index] = '"' + value + '"';
+        });
+
+        // join arrays of column names and values to add by commas and add them to our query string
+        mysql.connect()
+            .query('INSERT INTO ' + mysqlTableName + '(' + (databaseColumns.join(',')) + ') VALUES (' + valuesToAdd.join(',') + ')', 
+                // call user's callback function
+                function(err) {
+                    // get err param if any and pass it to callback before calling
+                    callback.call(mysql, err);
+                });
+    },
+
+    /**
+     * selects entries from table, using passed logic
+     *
+     * @param mysqlTableName    = {Object}      entry object from local 'database' object
+     * @param databaseColumns   = {Array}       containing names of mysql table columns to select
+     * @param whereLogic        = {String}      containing equality to use to target the selection of a specific row
+     * @param callback          = {Function}    to call after operation has completed successfully
+     *
+     * if @param whereLogic is 'null', all rows are selected and returned
+    **/
+    selectFrom : function(mysqlTableName, databaseColumns, whereLogic, callback) {
+        // perform query
+        mysql.connect()
+            .query('SELECT ' + databaseColumns.join(',') + ' FROM ' + mysqlTableName + ' WHERE ' + (whereLogic || '1 = 1'), callback);
+    },
+
+    /**
+     * updates entry in database table, using passed logic
+     *
+     * @param mysqlTableName    = {Object}      entry object from local 'database' object
+     * @param databaseColumns   = {Array}       containing names of mysql table columns to update values
+     * @param updatedValues     = {Array}       containing updated entry values
+     * @param whereLogic        = {String}      containing equality to use to target the update of a specific row
+     * @param callback          = {Function}    to call after operation has completed successfully
+    **/
+    update : function(mysqlTableName, databaseColumns, updatedValues, whereLogic, callback) {
+        // variable containing key value pairs to update from arrays passed
+        var keyValuePairs = '';
+
+        // generate and store key-value pairs from our two arrays
+        databaseColumns.forEach(function(column, index) {
+            // add to our string of pairs
+            keyValuePairs += ',' + column + ' = ' + '"' + updatedValues[index] + '"';
+        });
+
+        // strip comma from key value pairs string
+        keyValuePairs = keyValuePairs.substring(1);
+
+        // join arrays of column names and values to add by commas and add them to our query string
+        mysql.connect()
+            .query('UPDATE ' + mysqlTableName + ' SET ' + keyValuePairs + ' WHERE ' + (whereLogic || '1 = 1'), 
+                // call user's callback function
+                function(err) {
+                    // get err param if any and pass it to callback before calling and exit
+                    return callback.call(mysql, err);
+                });
+    }
 };
 
 // declare functions and methodical procedures
@@ -173,21 +345,6 @@ function handleRequestAsAPICall(request, response) {
 }
 
 /**
- * handles requets as receiving data with GET method
- */
-function handleRequestAsGETEndpoint(request, response) {
-
-    var requestData = request.url.split('/echo/')[1];
-
-    response.writeHead(SERVER_HEAD_OK, {
-        'Access-Control-Allow-Origin' : '*'
-    });
-
-    response.end(requestData);
-}
-
-
-/**
  * POSTs current api request to endpoint uri and returns response to client
  */
 function handleRequestAsAPIPOSTCall(request, response) {
@@ -242,7 +399,40 @@ function handleRequestAsAPIPOSTCall(request, response) {
 }
 
 /**
- * handle all requests formed as /post
+ * handles requets as receiving data with GET method
+ */
+function handleRequestAsGETEndpoint(request, response) {
+
+    var requestData = request.url.split('/echo/')[1];
+
+    response.writeHead(SERVER_HEAD_OK, {
+        'Access-Control-Allow-Origin' : '*'
+    });
+
+    response.end(requestData);
+}
+
+function handleRequestAsActionLogin(request, response) {
+
+    if(request.method != 'POST') {
+        response.writeHead(SERVER_HEAD_ERROR);
+        response.end(SERVER_RES_POST_ERROR);
+    }
+
+    var postData = '';
+
+    request.on('data', function(chunk) {
+        postData += chunk;
+    });
+
+    request.on('end', function() {
+        mysql.
+    });
+
+}
+
+/**
+ * handle all requests formed as /post and relay it back to the client
  */
 function handleRequestAsPostData(request, response) {
 
